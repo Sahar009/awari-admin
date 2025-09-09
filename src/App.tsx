ffc87f706026bebda963d93b0c001efc4c988e07
Hollajay <127396029+Hollajay@users.noreply.github.com>
2025-09-09 11:02:10 +0100

    admin vite project created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-6xl text-red-600'>Olajide</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
