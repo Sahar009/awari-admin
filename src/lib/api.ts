3f29e8170f80b7165cae4f5879a30c5fe2141eea
akinwumisehinde <akinwumisehinde@gmail.com>
2025-12-07 19:02:01 +0100

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000/api',
-   baseURL: 'http://localhost:8000/api',
+  baseURL: "https://awari-backend.onrender.com/api",
+  //  baseURL: 'http://localhost:8000/api',
   timeout: 15000
 });
 
